66ae2fbacefb6e590bfa5a341050009e445b1bfd
carlosaw <carlosfreevg@gmail.com>
2023-07-07 21:31:30 -0400

    States 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
 "use client";
 
+import { useState } from "react";
+
 const Page = () => {
+  
+  const [count, setCount] = useState(0);
 
-  let count = 0;
-
-  const handleClickButton = () => {
-    count++;
-    console.log(count);
+  const handleBtnClick = () => {    
+    setCount(count + 2);
+    const newCount = count;
+    console.log('o número novo é: ', newCount);
   }
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center mt-8">
+    <div className="w-screen h-screen flex flex-col justify-center items-center mt-8">      
       <p>{count}</p>
-      <button onClick={handleClickButton} className="bg-blue-700 p-3">+1</button>
+      <button onClick={handleBtnClick} className="bg-blue-700 rounded p-3">+2</button>
     </div>
   );
 }
