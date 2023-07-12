1848fa53e664c98f207cdaafd4b9cc4fc10a5ec0
carlosaw <carlosfreevg@gmail.com>
2023-07-11 20:04:16 -0400

    open

--- src/components/QuestionItem.tsx
+++ src/components/QuestionItem.tsx
@@ -25,11 +25,11 @@ export const QuestionItem = ({question, count, onAnswer}: Props) => {
           <div
             key={key}
             onClick={() => checkQuestion(key)}
-            className={`border px-3 py-2 rounded-md text-lg mb-4 cursor-pointer bg-blue-100 border-blue-300 cursor-pointer
-            
-            ${selectedAnswer !== null ? 'cursor-auto' : 'cursor-pointer hover:opacity-60'}
-            ${selectedAnswer !== null && selectedAnswer === question.answer && selectedAnswer === key && 'bg-green-100 border-green-300'}
-            ${selectedAnswer !== null && selectedAnswer !== question.answer && selectedAnswer === key && 'bg-red-100 border-red-300'}
+            className={
+            `border px-3 py-2 rounded-md text-lg mb-4 cursor-pointer bg-blue-100 border-blue-300 cursor-pointer            
+              ${selectedAnswer !== null ? 'cursor-auto' : 'cursor-pointer hover:opacity-60'}
+              ${selectedAnswer !== null && selectedAnswer === question.answer && selectedAnswer === key && 'bg-green-100 border-green-300'}
+              ${selectedAnswer !== null && selectedAnswer !== question.answer && selectedAnswer === key && 'bg-red-100 border-red-300'}
             `}
           >{item}</div>
         ))}
