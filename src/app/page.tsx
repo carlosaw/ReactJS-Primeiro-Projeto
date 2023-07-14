5aaa0b3ed8525ea0c263bc89e8ab52e2800c1e1a
carlosaw <carlosfreevg@gmail.com>
2023-07-14 17:01:53 -0400

    Reducers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,18 @@
 "use client";
 
-import { VideoPlayer } from "@/components/VideoPlayer";
-import { useState } from "react";
+import { listReducer } from "./reducers/listReducer";
+import { Item } from "@/types/Item";
+import { useReducer, useState } from "react";
 
 const Page = () => {
-  const [playing, setPlaying] = useState(false);
   
+  const [list, dispatch] = useReducer(listReducer, []);
+  
+
   return (    
     <div className="">      
-      <div className="border border-white p-3 mb-4">
-        <p className="text-2xl mb-3 text-blue-400">{playing ? 'RODANDO' : 'PAUSADO'}</p>
-        <button className="bg-blue-400 rounded-md p-3 text-black" onClick={() => setPlaying(!playing)}>Play/Pause</button>
-      </div>
-
-      <VideoPlayer
-        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-        isPlaying={playing}
-      />
+      
     </div>
-
   );    
 }
 
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,12 +1,22 @@
+import { useEffect } from "react";
+
 export const Square = () => {
+
+  useEffect(() => {
+    // Roda quando o componente aparece na tela. (Conecta)
+       console.log("CONECTADO");
+
+    // Roda quando o componente some da tela. (Desconecta)
+    return () => {
+      console.log("desconectado");
+    }
+  });
+
   return (
     <div className="
       w-52 h-52
       bg-orange-700
-      text-white 
-      flex 
-      items-center
-      justify-center
+      text-white
     ">
       Texto
     </div>
