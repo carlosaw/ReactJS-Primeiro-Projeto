5b4d092ab2f9d3a58db0d054c50e6d99fe02f097
carlosaw <carlosfreevg@gmail.com>
2023-07-13 21:28:31 -0400

    VideoPlayer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,25 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { VideoPlayer } from "@/components/VideoPlayer";
+import { useState } from "react";
 
 const Page = () => {
+  const [playing, setPlaying] = useState(false);
   
-  /*
-  1. Definição da FUNÇÃO que vai rodar.
-  2. Definição de QUANDO vai rodar.
-  3. Definição do que fazer quando o componente sumir/unload.
-  */
-  const [name, setName] = useState('Carlos');
-  const [age, setAge] = useState(90);
-  /*Monitora somente [name]*/
-  // useEffect(() => {
-  //   console.log('Rodou o Effect')
-  // }, [name]);
-  
-  /* Monitora todos*/
-  useEffect(() => {
-    console.log('Rodou o Effect')
-  }, []);
+  return (    
+    <div className="">      
+      <div className="border border-white p-3 mb-4">
+        <p className="text-2xl mb-3 text-blue-400">{playing ? 'RODANDO' : 'PAUSADO'}</p>
+        <button className="bg-blue-400 rounded-md p-3 text-black" onClick={() => setPlaying(!playing)}>Play/Pause</button>
+      </div>
 
-  // const handlePedroClick = () => {
-  //   setName('Pedro');
-  // }
-
-  return (
-    
-    <div className="w-full h-screen">      
-      <p>Meu nome é: {name} e eu tenho {age} anos.</p>
-    <hr/>
-      <button className="border border-bg-400 m-3 p-3" onClick={()=>setName('Pedro')}>Mudar para Pedro</button>
-      <button className="border border-bg-400 m-3 p-3" onClick={()=>setName('João')}>Mudar para João</button>
-      <button className="border border-bg-400 m-3 p-3" onClick={()=>setAge(10)}>Mudar para 10 anos</button>
-      <button className="border border-bg-400 m-3 p-3" onClick={()=>setAge(90)}>Mudar para 90 anos</button>     
+      <VideoPlayer
+        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+        isPlaying={playing}
+      />
     </div>
+
   );    
 }
 
-<script src="http://localhost:3000"></script>
 export default Page;
